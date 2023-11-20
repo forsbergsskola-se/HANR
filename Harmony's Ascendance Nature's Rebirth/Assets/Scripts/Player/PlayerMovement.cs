4b2c7ea346ac642e15d348bc416fa72ecb63f96d
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-11-20 11:56:13 +0100

    Update player movement script

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerMovement.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,17 +10,48 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         private GameObject player;
+        private Vector3 mousePosition;
+        private Vector3 playerDestination;
+        [SerializeField] private float smooth;
+        
         
     
-      void Start()
-      {
-          player = GameObject.FindWithTag("Player");
-      }
+        void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
 
-    
-      void Update()
-      {
+        private void Update()
+        {
+            FindCursorPos();
+        }
+
+        private void FixedUpdate()
+        {
+            RotatePlayer();
+            MoveToCursorPos();
+        }
+
+        private void FindCursorPos()
+        {
+            mousePosition = Input.mousePosition;
+        }
+
+        private void RotatePlayer()
+        {   // where is the player looking now, where is the mouse, update looking direction
+            float result = Vector3.Angle(player.transform.forward, mousePosition);
+            Quaternion target = Quaternion.Euler(0, result, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            
+        }
         
-      } 
+        private void MoveToCursorPos()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                
+            }
+        }
+
     }
 }
