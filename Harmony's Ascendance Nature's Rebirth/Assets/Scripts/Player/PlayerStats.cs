1a2a76eeb5cfde8ea235583369e405ed91a3001b
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-11-20 11:55:49 +0100

    Fixed Level UI

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerStats.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerStats.cs
@@ -6,4 +6,7 @@ public class PlayerStats : MonoBehaviour
 {
     public float health = 100;
     public float magic = 50;
+    public float exp = 0;
+    public int level = 1;
+    
 }
--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/PlayerHud.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/PlayerHud.cs
@@ -8,11 +8,15 @@ public class PlayerHud : MonoBehaviour
 {
     public TMP_Text healthText;
     public TMP_Text magicText;
+    public TMP_Text expText;
+    public TMP_Text levelText;
 
     private void Update()
     {
         healthText.text = "Health:" + GetComponent<PlayerStats>().health;
         magicText.text = "Magic: " + GetComponent<PlayerStats>().magic;
+        expText.text = "Exp: " + GetComponent<PlayerStats>().exp; 
+        levelText.text = "Level: " + GetComponent<PlayerStats>().level;
     }
 }
 
