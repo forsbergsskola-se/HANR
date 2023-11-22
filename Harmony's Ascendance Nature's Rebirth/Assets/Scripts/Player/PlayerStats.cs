4f61e9b805bbb82f63b3c323327125d9441ae844
reynin <144011447+reynin@users.noreply.github.com>
2023-11-22 10:59:49 +0100

    Update playerHUD

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerStats.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerStats.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "Stats", menuName = "Mall/Create Mall")]
 public class PlayerStats : ScriptableObject
 {
+    public UnityEvent UpdatePlayerHUD = new UnityEvent();
+    
     public float health;
     public float magic;
     public float exp;
     public int level;
-    
-    
+
+    public void setValueHealth(float newValue)
+    {
+        health = newValue;
+        UpdatePlayerHUD.Invoke();
+    }
 }
