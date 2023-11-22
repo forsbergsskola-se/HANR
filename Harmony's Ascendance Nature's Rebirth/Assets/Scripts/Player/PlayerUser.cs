00267f8cab2a6884bd27489f0ed6622e2491a825
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-11-22 11:32:35 +0100

    tested to create a new player scriptable objects assets

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerUser.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/PlayerUser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -21,5 +22,11 @@ public class PlayerUser : MonoBehaviour
             playerStats.exp -= 100;
             playerStats.level++;
         }
+        
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        playerStats.setValueExp(70);
     }
 }
