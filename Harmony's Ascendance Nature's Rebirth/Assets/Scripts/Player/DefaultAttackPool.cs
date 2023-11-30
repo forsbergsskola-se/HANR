3880108f7020e3d7e5cff51d0ef87aa7be13d672
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-11-30 13:05:59 +0100

    Add methods to DefaultAttackPool

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/DefaultAttackPool.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/DefaultAttackPool.cs
@@ -1,9 +1,43 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Player
 {
     public class DefaultAttackPool : MonoBehaviour
     {
+        public GameObject daEffect;
+        public int size;
+
+        private List<GameObject> daEffectPool;
+
+        private void Start()
+        {
+            daEffectPool = new List<GameObject>();
+            SetUpPool();
+        }
         
+        private void SetUpPool()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                GameObject effect = Instantiate(daEffect);
+                effect.SetActive(false);
+                daEffectPool.Add(effect);
+            }
+        }
+        
+        public GameObject GetPooledEffects()
+        {
+            foreach (GameObject effect in daEffectPool)
+            {
+                if (!effect.activeInHierarchy)
+                {
+                    effect.SetActive(true);
+                    return effect;
+                }
+            }
+            return null;
+        }
     }
 }
