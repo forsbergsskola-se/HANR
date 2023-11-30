4554a3b5515326b0852f4ec04962c4c5f2930c54
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-11-30 10:46:19 +0100

    VicinityCheck And PickUp

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Objects/ItemPickUp.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Objects/ItemPickUp.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class ItemPickUp : MonoBehaviour
 {
     public InventoryHolder inventoryHolder;
-    public Item item;
-
-    private void VicinityCheck()
-    {
-        //check if player is close enough for pickup
-    }
     
     public void PickUp(Item item)
     {
-        //TODO add item to inventoryHolder List this item:)
+        inventoryHolder.Items.Add(item);
+        inventoryHolder.pickUp.Invoke();
     }
 }
