830d6edf7a514a44758c4b528538f8b925ba570d
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-11-30 11:27:58 +0100

    ItemPickUp and ItemHolder Finished.

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Objects/ItemPickUp.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Objects/ItemPickUp.cs
@@ -1,14 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ItemPickUp : MonoBehaviour
 {
-    public InventoryHolder inventoryHolder;
+    private InventoryHolder inventoryHolder;
+    public Item item;
+    private bool Inside;
     
-    public void PickUp(Item item)
+    private void Start()
+    {
+        inventoryHolder = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryHolder>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G) && Inside)
+        {
+            Debug.Log("Pickup");
+            
+            //Adds item Pickup
+            PickUp();
+            
+            Destroy(gameObject);
+          
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+          
+            Debug.Log("true");
+            Inside = true;
+          
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Inside = false;
+        }
+    }
+    
+    public void PickUp()
     {
         inventoryHolder.Items.Add(item);
         inventoryHolder.pickUp.Invoke();
     }
+
 }
