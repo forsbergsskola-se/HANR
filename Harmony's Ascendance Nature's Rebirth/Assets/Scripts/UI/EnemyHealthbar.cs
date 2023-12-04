168940614c3707391a62929b1d9ea130e99d0b5d
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-12-04 14:29:43 +0100

    Terrain manipulation test

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/EnemyHealthbar.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/EnemyHealthbar.cs
@@ -1,14 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using CustomObjects;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthbar : MonoBehaviour
 {
     public FloatVariable enemyHealth;
     public Camera screen;
+
+    private Slider enemyHealthSlider;
+    private float maxHealth;
+
+    private void Awake()
+    {
+        SetUpSlider();
+        enemyHealth.ValueChanged.AddListener(UpdateHealthBar);
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        enemyHealth.ValueChanged.RemoveListener(UpdateHealthBar);
+    }
+
     void Update()
     {
         transform.LookAt(screen.transform);
     }
+
+
+    private void SetUpSlider()
+    {
+        enemyHealthSlider.minValue = 0;
+        enemyHealthSlider.maxValue = 100;
+    }
+
+private void UpdateHealthBar(float arg0)
+    {
+        
+    }
+
+    
 }
