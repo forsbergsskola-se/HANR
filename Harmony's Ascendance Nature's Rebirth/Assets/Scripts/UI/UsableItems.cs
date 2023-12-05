b5783605ade2d9128a71f91df1a3a8a96d824912
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-12-05 14:48:40 +0100

    Pushes

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/UsableItems.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/UsableItems.cs
@@ -38,35 +38,35 @@ public class UsableItems : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha1) && inventoryHolder.Items[0] != defaultItem)
         {
-            Debug.Log("Key 1 pressed");
+            
             CheckSlot(0);
 
         }
 
         else if (Input.GetKeyDown(KeyCode.Alpha2) && inventoryHolder.Items[1] != defaultItem)
         {
-            Debug.Log("Key 2 pressed");
+            
             CheckSlot(1);
 
         }
 
         else if (Input.GetKeyDown(KeyCode.Alpha3) && inventoryHolder.Items[2] != defaultItem)
         {
-            Debug.Log("Key 3 pressed");
+            
             CheckSlot(2);
 
         }
 
         else if (Input.GetKeyDown(KeyCode.Alpha4) && inventoryHolder.Items[3] != defaultItem)
         {
-            Debug.Log("Key 4 pressed");
+          
             CheckSlot(3);
 
         }
 
         else if (Input.GetKeyDown(KeyCode.Alpha5) && inventoryHolder.Items[4] != defaultItem)
         {
-            Debug.Log("Key 5 pressed");
+        
             CheckSlot(4);
 
         }
