8c7182ed3d8fb19c5b314fc0e0c1d0ea251ca362
meet-rakholia <meet@student.forsbergsskola.se>
2023-12-05 07:33:56 +0100

    added boss enemy death animation

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/NPC/Enemy/BossEnemy/BossEnemyTakeDamage.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/NPC/Enemy/BossEnemy/BossEnemyTakeDamage.cs
@@ -2,20 +2,28 @@ using System;
 using CustomObjects;
 using Player;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Enemy.BossEnemy
 {
     public class BossEnemyTakeDamage : MonoBehaviour
     {
         public FloatVariable enemyHealth;
+        [SerializeField]private Animator animator;
+        [SerializeField] private NavMeshAgent agent;
 
         private void OnTriggerEnter(Collider other)
         {
             ProjectileStats ps = other.gameObject.GetComponent<ProjectileStats>();
             float damage = ps.attackDamage;
             float currentHealth = enemyHealth.getValue();
-            enemyHealth.setValue(currentHealth - damage);
+            enemyHealth.setValue(Mathf.Max(currentHealth - damage,0));
             other.gameObject.SetActive(false);
+            if (enemyHealth.getValue() <= 0)
+            {
+                animator.SetBool("isDead",true);
+                agent.isStopped = true;
+            }
         }
     }
 }
