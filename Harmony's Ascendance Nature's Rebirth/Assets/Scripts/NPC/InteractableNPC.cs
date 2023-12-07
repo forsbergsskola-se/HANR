9fe78b9bcec6a5ad9ae992aaa9f857c5e1e66a1b
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-12-07 16:40:16 +0100

    Dialog (work in progress)

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/NPC/InteractableNPC.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/NPC/InteractableNPC.cs
@@ -7,11 +7,19 @@ using UnityEngine;
 public class InteractableNPC : MonoBehaviour
 {
     public Dialogue dialogue;
-    private void OnTriggerStay(Collider other)
+    private bool playerClose;
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && Input.GetKeyDown(KeyCode.Space)) //If player is close enough, press right mouse to trigger dialogue
-        { 
-            Debug.Log("Hello");
+        if (other.CompareTag("Player"))
+        {
+            playerClose = true;
+        }
+        
+    }
+    private void OnTriggerStay(Collider other) //TODO code runs twice?
+    {
+        if(playerClose && Input.GetKeyDown(KeyCode.Space)) //If player is close enough, press right mouse to trigger dialogue
+        {
             dialogue.druidToRanger.Invoke();
         }
     }
--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/Dialogue.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/Dialogue.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,21 +11,76 @@ public class Dialogue : MonoBehaviour
 {
     public UnityEvent druidToRanger;
     public TMP_Text chating;
+    private int dialougeCounter;
+
+    public GameObject PlayerUI;
+
+    private bool isInDialogue;
+    private bool keyPressed;
+
+    private string[] conversation = new string[4];
+
     private void Start()
-    {   
+    {
         this.gameObject.SetActive(false);
-        druidToRanger.AddListener(DruidTalkingRanger);
+        druidToRanger.AddListener(InitiateDialogue);
+        FillArray();
     }
 
     private void OnDestroy()
     {
-        druidToRanger.RemoveListener(DruidTalkingRanger);
+        druidToRanger.RemoveListener(InitiateDialogue);
     }
 
-    void DruidTalkingRanger()
+    private void FillArray()
     {
-        GameObject.Find("PlayerUICanvas").SetActive(false);
+        conversation[0] =
+            "I'm distraught, the water in these woods are vital for all life but has been curated by darkness and is slowly killing everything that is dependent on it.";
+
+        conversation[1] = "text2";
+        conversation[2] = "text3";
+        conversation[3] = "text4";
+
+    }
+
+    private void InitiateDialogue()
+    {
+        PlayerUI.SetActive(false);
         this.gameObject.SetActive(true);
-        chating.text = "I'm distraught, the water in these woods are vital for all life but has been curated by darkness and is slowly killing everything that is dependent on it.";
+        dialougeCounter = 0;
+        
+        StartCoroutine(WaitForKeyPress());
+    }
+    
+    private IEnumerator WaitForKeyPress()
+    {
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return null;
+        }
+        UpdateDialogue();
+    }
+
+    private void UpdateDialogue()
+    {
+        if (dialougeCounter == 0)
+        {
+            chating.text = conversation[0];
+            dialougeCounter++;
+        }
+        if (dialougeCounter == 1)
+        {
+            chating.text = conversation[1];
+            dialougeCounter++;
+        }
+        if (dialougeCounter == 2)
+        {
+            chating.text = conversation[2];
+            dialougeCounter++;
+        }
+        if (dialougeCounter == 3)
+        {
+            chating.text = conversation[3];
+        }
     }
 }
