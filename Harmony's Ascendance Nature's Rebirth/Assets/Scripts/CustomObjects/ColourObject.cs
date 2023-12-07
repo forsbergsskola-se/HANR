326625b8dcff4c337a1cb1bae282c4897dd6199d
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-12-07 12:56:39 +0100

    Easily Changeable Enviroment

---
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/CustomObjects/ColourObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Colour", menuName = "Colour", order = 0)]
+public class ColourObject : ScriptableObject
+{
+    public Color color;
+    
+}
