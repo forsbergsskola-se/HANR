cc0a6f34aba913cd85f87659ba2392f4300c5aa7
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-12-11 19:19:20 +0100

    ColourChange

---
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Enviromental/ColourTriggerChange.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+
+public class ColourTriggerChange : MonoBehaviour
+{
+    private Light mainLight;
+    private Coroutine colorTransitionCoroutine;
+
+    [SerializeField] private float transitionDuration = 2.0f;
+    [SerializeField] private Color bossColor;
+    [SerializeField] private Color normalColor;
+
+    private bool isTransitioning = false;
+
+    void Start()
+    {
+        mainLight = GameObject.FindWithTag("MainLight")?.GetComponent<Light>();
+
+        if (mainLight == null)
+        {
+            Debug.LogError("Main light not found. Make sure it has the 'MainLight' tag and a Light component.");
+        }
+    }
+
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        // Start the boss color transition coroutine and store a reference to it
+        colorTransitionCoroutine = StartCoroutine(LerpColorboss(bossColor));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Stop the boss color transition coroutine if it's running
+        if (colorTransitionCoroutine != null)
+        {
+            StopCoroutine(colorTransitionCoroutine);
+        }
+
+        // Start the normal color transition coroutine
+        StartCoroutine(LerpColor(normalColor));
+    }
+
+
+    IEnumerator LerpColorboss(Color targetColor)
+    {
+        isTransitioning = true;
+
+        Color initialColor = mainLight.color;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitionDuration)
+        {
+            mainLight.color = Color.Lerp(initialColor, targetColor, elapsedTime / transitionDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        mainLight.color = targetColor;
+        isTransitioning = false;
+    }
+    IEnumerator LerpColor(Color targetColor)
+    {
+
+        Color initialColor = mainLight.color;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitionDuration)
+        {
+            mainLight.color = Color.Lerp(initialColor, targetColor, elapsedTime / transitionDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        mainLight.color = targetColor;
+      
+    }
+    
+}
