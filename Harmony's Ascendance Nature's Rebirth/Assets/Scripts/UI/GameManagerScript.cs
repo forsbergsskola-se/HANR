d227051acf2ca76e5134880a7dd30481266adec3
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-12-12 14:20:10 +0100

    created start scene and updated Game over function

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/GameManagerScript.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/GameManagerScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
 public class GameManagerScript : MonoBehaviour
@@ -9,5 +10,20 @@ public class GameManagerScript : MonoBehaviour
     void Start()
     {
         this.gameObject.SetActive(false);
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
