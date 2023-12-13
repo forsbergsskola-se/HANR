0632deb22446a075e8b3ccfcb818a5bb49479539
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-12-13 14:45:33 +0100

    Camera Zoom Npc

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/CameraScript.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/Player/CameraScript.cs
@@ -1,3 +1,5 @@
+using System;
+using NPC;
 using UnityEngine;
 
 namespace Player
@@ -6,14 +8,24 @@ namespace Player
     {
         [SerializeField] private GameObject cameraObject;
         private Transform cameraPos;
-        private Transform playerTransform; // Added playerTransform variable
+        private Transform playerTransform;
+        private bool locked;
+        private bool talkingBearMan;
+        private bool talkingRanger;
 
+
+        [SerializeField] float DialoguezoomSpeed = 5f;
+        [SerializeField] private float zoomDistance = 15f;
         [SerializeField] private float rotationSpeed = 3f;
-        [SerializeField] private float zoomSpeed = 5f; // Adjust as needed
-        [SerializeField] private float minZoom = 20f; // Adjust as needed
+        [SerializeField] private float zoomSpeed = 5f;
+        [SerializeField] private float minZoom = 20f;
         [SerializeField] private float maxZoom = 60f;
-        [SerializeField] private float verticalOffset = 2f; // Adjust as needed
+        [SerializeField] private float verticalOffset = 2f;
         [SerializeField] private LayerMask obstacleLayer;
+        [SerializeField] private Dialogue dialogue;
+        [SerializeField] private Transform Ranger;
+        [SerializeField] private Transform BearMan;
+        [SerializeField] private Vector3 Dialogueoffset = new Vector3(0, 5, 0);
 
         private void Start()
         {
@@ -25,22 +37,43 @@ namespace Player
             }
 
             cameraPos = cameraObject.transform;
-            playerTransform = transform; // Assuming the script is attached to the player object
+            playerTransform = transform;
+
+            dialogue.druidToRanger.AddListener(lockCameraRanger);
+            dialogue.druidToBearMan.AddListener(lockCameraBearman);
+
+        }
+
+        private void OnDestroy()
+        {
+            dialogue.druidToRanger.RemoveListener(lockCameraRanger);
+            dialogue.druidToBearMan.RemoveListener(lockCameraBearman);
         }
 
         void Update()
         {
-            HandleRotationInput();
-            HandleZoomInput();
 
-            HandleObstacleVisibility();
+            if (!locked)
+            {
+                HandleRotationInput();
+                HandleZoomInput();
+                HandleObstacleVisibility();
+                UpdateCameraPosition();
+            }
+            else if (talkingBearMan)
+            {
+                CameraBearMan();
+            }
+            else if (talkingRanger)
+            {
+                CameraRanger();
+            }
 
-            UpdateCameraPosition(); // Added method to update camera position
         }
 
         private void HandleRotationInput()
         {
-            if (Input.GetMouseButton(0)) // Left mouse button to rotate
+            if (Input.GetMouseButton(0))
             {
                 float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed;
                 cameraPos.RotateAround(playerTransform.position, Vector3.up, horizontalInput);
@@ -70,10 +103,71 @@ namespace Player
 
         private void UpdateCameraPosition()
         {
-            // Update camera position to follow the player with an offset
+
             Vector3 offset = Quaternion.Euler(0, cameraPos.eulerAngles.y, 0) * new Vector3(0, verticalOffset, -10);
             Vector3 targetPosition = playerTransform.position + offset;
             cameraPos.position = Vector3.Lerp(cameraPos.position, targetPosition, Time.deltaTime * 5f);
+        }
+
+        private void lockCameraRanger()
+        {
+            locked = true;
+            talkingRanger = true;
+        }
+
+        private void lockCameraBearman()
+        {
+            locked = true;
+            talkingBearMan = true;
+        }
+
+        private void UnLock()
+        {
+            locked = false;
+        }
+
+        private void CameraRanger()
+        {
+            ZoomIn(Ranger);
+
+            void ZoomIn(Transform character)
+            {
+
+                if (character != null)
+                {
+                    Vector3 targetPosition = character.position + (Vector3.forward * zoomDistance + Dialogueoffset);
+
+                    // Use Lerp for smooth camera movement
+                    cameraObject.transform.LookAt(character);
+                    cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, targetPosition,
+                        Time.deltaTime * DialoguezoomSpeed);
+
+                }
+            }
+
+            locked = true;
+        }
+
+        private void CameraBearMan()
+        {
+            ZoomIn(BearMan);
+
+            void ZoomIn(Transform character)
+            {
+
+                if (character != null)
+                {
+                    Vector3 targetPosition = character.position + (Vector3.forward * zoomDistance + Dialogueoffset);
+
+                    // Use Lerp for smooth camera movement
+                    cameraObject.transform.LookAt(character);
+                    cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, targetPosition,
+                        Time.deltaTime * DialoguezoomSpeed);
+
+                }
+            }
+
+            locked = true;
         }
     }
 }
