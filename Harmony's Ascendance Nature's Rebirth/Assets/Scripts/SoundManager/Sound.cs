fd501d3cb9942f4a3d4d2f4b11088e15f01edced
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-12-15 16:20:28 +0100

    Scripting sound manager

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/SoundManager/Sound.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/SoundManager/Sound.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 namespace SoundManager
 {
-    public class Sound : MonoBehaviour
+    [System.Serializable]
+    public class Sound
     {
-        
+        public string soundName;
+        public AudioClip audioClip;
+
+
+        [Range(0f, 1f)]
+        public float volume = 1f;
+
+        public bool loop;
+
+
+        [HideInInspector]
+        public AudioSource source;
     }
 }
--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/SoundManager/SoundManager.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/SoundManager/SoundManager.cs
@@ -1,9 +1,90 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace SoundManager
 {
     public class SoundManager : MonoBehaviour
     {
+        public float soundVolume = 0.75f;
+        static SoundManager i;
+        public Sound[] sounds;
+
+
+        private void Awake()
+        {
+            i = this;
+
+            foreach (Sound sound in sounds)
+            {
+                sound.source = gameObject.AddComponent<AudioSource>();
+                sound.source.clip = sound.audioClip;
+                sound.source.volume = sound.volume * soundVolume;
+                sound.source.loop = sound.loop;
+            }
+        }
+
+        private void Start()
+        {
+            if (SceneManager.GetActiveScene().name == "Game Scene 1")
+            {
+                PlaySound("Game Music");
+            }
+
+            if (SceneManager.GetActiveScene().name == "Main Menu")
+            {
+                PlaySound("Menu Music");
+            }    
+        }
         
+        Sound GetSound(string soundName)
+        {
+            Sound sound = Array.Find(sounds, sound => sound.soundName == soundName);
+            Sound soundAlt = Array.Find(sounds, sound => sound.audioClip.name == soundName);
+
+            if (sound != null)
+            {
+                // sound.source.Stop();
+                // sound.source.Play();
+                return sound;
+            }
+            if(soundAlt != null)
+            {
+                // soundAlt.source.Stop();
+                // soundAlt.source.Play();
+                return soundAlt;
+            }
+    
+            Debug.Log("Can't Find: " + soundName);
+            return null;
+        }
+
+
+        public static void PlaySound(string soundName)
+        {
+            Sound sound = i.GetSound(soundName);
+            
+            if(sound == null)
+                return;
+            sound.source.Stop();
+            sound.source.Play();
+        }
+        
+        /*
+        public static void StopSound(string soundName)
+        {
+            Sound sound = i.GetSound(soundName);
+            if(sound == null)
+                return;
+            sound.source.Stop();
+        }
+
+        public static void SetVolume(string soundName, float volume)
+        {
+            Sound sound = i.GetSound(soundName);
+            if(sound == null)
+                return;
+            sound.source.volume = volume;
+        }*/
     }
 }
