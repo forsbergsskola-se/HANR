1732fb52fc4a23af1e0c845ae38328c28ebe95d2
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-12-15 16:15:09 +0100

    Updated + added Quest scripts + face images

--- Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/Dialogue.cs
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/Dialogue.cs
@@ -9,6 +9,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
@@ -27,7 +28,7 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private NavMeshAgent agent;
     public string[] conversation = new string[5];
     private bool inConversation;
-    public QuestUI questUI;
+    public Quest quest;
 
     
     private void Start()
@@ -44,7 +45,7 @@ public class Dialogue : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                questUI.gameObject.SetActive(false);
+                quest.gameObject.SetActive(false);
                 
                 if (dialougeCounter > conversation.Length-1) //End conversation
                 {
@@ -52,26 +53,30 @@ public class Dialogue : MonoBehaviour
                     dialougeCounter = 0;
                     this.gameObject.SetActive(false);
                     PlayerUI.SetActive(true);
-                    questUI.gameObject.SetActive(true);
+                    quest.gameObject.SetActive(true);
 
-                    switch (questUI.currentState)
+                    switch (quest.currentWaterStaffState)
                     {
-                        case QuestUI.QuestLine.TalkingToRanger:
-                            questUI.questProgression.Invoke(1); //State goes to next (FindingBearMan)
+                        case Quest.WaterStaffQuestLine.TalkingToRanger:
+                            quest.questProgression.Invoke(1); //State goes to next (FindingBearMan)
                             break;
                             
-                        case QuestUI.QuestLine.FindingBearMan:
-                            questUI.questProgression.Invoke(2); //State goes to next (CollectingCrate)
+                        case Quest.WaterStaffQuestLine.FindingBearMan:
+                            quest.questProgression.Invoke(2); //State goes to next (CollectingCrate)
+                            break;
+                        case Quest.WaterStaffQuestLine.GoBackToRanger: 
+                            quest.questProgression.Invoke(7); //End questline
                             break;
                     }
                     
                     currentlySpeaking.sprite = null;
                 }
-                else
+                else //Dialogue continues
                 {
-                    chating.text = conversation[dialougeCounter]; // Continue conversation
-
-                    if (questUI.currentState == QuestUI.QuestLine.TalkingToRanger) // When talking to ranger first time
+                    chating.text = conversation[dialougeCounter];
+                    
+                    //For dialogue face images
+                    if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.TalkingToRanger) // When talking to ranger first time
                     {
                         if (chating.text == conversation[1] || chating.text == conversation[3])
                         {
@@ -83,8 +88,12 @@ public class Dialogue : MonoBehaviour
                             currentlySpeaking.sprite = faceRanger;
                         }
                     }
+                    else if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.GoBackToRanger)
+                    {
+                        currentlySpeaking.sprite = faceRanger;
+                    }
 
-                    if (questUI.currentState == QuestUI.QuestLine.FindingBearMan)
+                    if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.FindingBearMan)
                     {
                         if (chating.text == conversation[1] || chating.text == conversation[3])
                         {
@@ -96,7 +105,19 @@ public class Dialogue : MonoBehaviour
                             currentlySpeaking.sprite = faceBearMan;
                         }
                     }
-                        
+
+                    if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.GoBackToRanger)
+                    {
+                        if (chating.text == conversation[1] || chating.text == conversation[3])
+                        {
+                            currentlySpeaking.sprite = faceDruid;
+                        }
+                        else if (chating.text == conversation[2] ||
+                                 chating.text == conversation[4])
+                        {
+                            currentlySpeaking.sprite = faceRanger;
+                        }
+                    }
                     dialougeCounter += 1;
                 }
             }
@@ -141,48 +162,61 @@ public class Dialogue : MonoBehaviour
         
     }
 
-    private void FillArrayRanger()
+    private void FillArrayRanger() //Dialogue lines
     {
-        if (questUI.currentState == QuestUI.QuestLine.TalkingToRanger)
+        if (quest.activeWaterStaffQuest)
         {
-            conversation[0] = "I'm distraught, the water in these woods are vital for all life but has been corrupted by darkness and is slowly killing everything that is dependent on it.";
-            conversation[1] = "Oh hello! What do you mean? Who corrupted the water?";
-            conversation[2] = "Dark forces has spread over these lands lately, it came from deep in the mountains... Those fire rocks are so evil, this forest used to be so beautiful!";
-            conversation[3] = "I know some magic, maybe I can help?";
-            conversation[4] = "There is a tale of a purifying spell crafted by the Bear Man. He keeps to himself and can be hard to find, but I've heard that he likes carving runes in to stone...";
-        }
-        else if (questUI.currentState != QuestUI.QuestLine.TalkingToRanger) //If Druid retriggers the conversation again
-        {
-            conversation[0] = "Go find the BearMan please!";
-            dialougeCounter = 4; //To cut the dialogue short
-        }
-        else if (questUI.currentState == QuestUI.QuestLine.EndQuest)
-        {
-            conversation[0] = "You have a staff now! Now you can fight the monsters!";
-            dialougeCounter = 4; //To cut the dialogue short
+            if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.TalkingToRanger)
+            {
+                conversation[0] = "I'm distraught, the water in these woods are vital for all life but has been corrupted by darkness and is slowly killing everything that is dependent on it.";
+                conversation[1] = "Oh hello! What do you mean? Who corrupted the water?";
+                conversation[2] = "Dark forces has spread over these lands lately, it came from deep in the mountains... Those fire rocks are so evil, this forest used to be so beautiful!";
+                conversation[3] = "I know some magic, maybe I can help?";
+                conversation[4] = "There is a tale of a purifying spell crafted by the Bear Man. He keeps to himself and can be hard to find, but I've heard that he likes carving runes in to stone...";
+            }
+
+            else if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.GoBackToRanger)
+            {
+                conversation[0] = "Thank you so much for your help!";
+                conversation[1] = "Don't mention it! Your home looks so beautiful now that the river is clean.";
+                conversation[2] = "It used to always look at this, but after the this stone creature woke up inside the mountain the forest turned dark.";
+                conversation[3] = "What are those stone creatures?";
+                conversation[4] = "Follow my friend Slimy, he'll show you where it resides.";
+            }
+            else if (quest.currentWaterStaffState != Quest.WaterStaffQuestLine.TalkingToRanger) //If player tries to retrigger the conversation again
+            {
+                conversation[0] = "Go find the BearMan please!";
+                dialougeCounter = 4; //To cut the dialogue short
+            }
         }
     }
 
-    private void FillArrayBearMan()
+    private void FillArrayBearMan() //Dialogue lines
     {
-        if (questUI.currentState == QuestUI.QuestLine.FindingBearMan)
+        if (quest.activeWaterStaffQuest)
         {
-            conversation[0] = "Rawr! Who goes there?";
-            conversation[1] = "Don't be alarmed, I'm a friend! I'm searching for a spell to purify the river by the Rangers grounds";
-            conversation[2] = "Oh, you're a friend of little Ranger? In that case I may be able to help you. It's been a long time since I used magic, other than to carve my masterpieces.";
-            conversation[3] = "There should be some crates with spell books and potions by the pine trees, feel free to have a look!";
-            conversation[4] = "Thank you";
-        }
-        else if (questUI.currentState != QuestUI.QuestLine.FindingBearMan)//If Druid goes to BearMan to soon
-        {
-            conversation[0] = "Growl! GET AWAY FROM ME!";
-            dialougeCounter = 4; //To cut the dialogue short
-        }
-        else if (questUI.currentState == QuestUI.QuestLine.EndQuest)
-        {
-            conversation[0] = "You have the Water Staff!";
-            conversation[1] = "Now go save the forest!";
-            dialougeCounter = 4; //To cut the dialogue short
+            if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.FindingBearMan)
+            {
+                conversation[0] = "Rawr! Who goes there?";
+                conversation[1] =
+                    "Don't be alarmed, I'm a friend! I'm searching for a spell to purify the river by the Rangers grounds";
+                conversation[2] =
+                    "Oh, you're a friend of little Ranger? In that case I may be able to help you. It's been a long time since I used magic, other than to carve my masterpieces.";
+                conversation[3] =
+                    "There should be some crates with spell books and potions by the pine trees, feel free to have a look!";
+                conversation[4] = "Thank you";
+            }
+            else if (quest.currentWaterStaffState !=
+                     Quest.WaterStaffQuestLine.FindingBearMan) //If Druid goes to BearMan to soon
+            {
+                conversation[0] = "Growl! GET AWAY FROM ME!";
+                dialougeCounter = 4; //To cut the dialogue short
+            }
+            else if (quest.currentWaterStaffState == Quest.WaterStaffQuestLine.GoBackToRanger)
+            {
+                conversation[0] = "You have the Water Staff Now go save the forest!";
+                dialougeCounter = 4; //To cut the dialogue short
+            }
         }
     }
 }
---
+++ Harmony's Ascendance Nature's Rebirth/Assets/Scripts/UI/Quest.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+
+namespace UI
+{
+    public class Quest : MonoBehaviour
+    {
+        public enum WaterStaffQuestLine
+        {
+            TalkingToRanger,
+            FindingBearMan,
+            CollectingCrate, //Spawn crates inventory (potions)
+            FindingRiverByRangerArea,
+            SaveTheRiver,
+            GettingReward, //Spawn staff
+            GoBackToRanger,
+            EndQuest
+        }
+        
+        public enum BossQuestLine
+        {
+            FindBossArea,
+            DefeatBoss,
+            ReturnToRanger
+        }
+
+        public bool activeWaterStaffQuest;
+        public bool activeBossQuest;
+        
+        public UnityEvent <int> questProgression; //Invoke this Unity-event during play-through
+
+        public TMP_Text questTitle;
+        public TMP_Text questLog;
+        public WaterStaffQuestLine currentWaterStaffState;
+        public BossQuestLine currentBossState;
+        void Start()
+        {
+            activeWaterStaffQuest = true;
+            currentWaterStaffState = WaterStaffQuestLine.TalkingToRanger;
+            questProgression.AddListener(SetState);
+        }
+
+        private void OnDestroy()
+        {
+            questProgression.RemoveListener(SetState);
+        }
+
+        private void Update()
+        {
+            if (activeWaterStaffQuest)
+            {
+                switch (currentWaterStaffState)
+                {
+                    case WaterStaffQuestLine.TalkingToRanger: //First Objective
+                        questTitle.text = "Talk to Ranger";
+                        questLog.text = "¤ Ranger is most likely to reside at a camp."; //Start
+                        break;
+                    case WaterStaffQuestLine.FindingBearMan: //1
+                        questTitle.text = "Find and Talk To the BearMan";
+                        questLog.text =
+                            "¤ I need to find this BearMan. But it's a big forest, he can be anywhere. Wonder if he left a teleport nearby for quicker traversal?";
+                        break;
+                    case WaterStaffQuestLine.CollectingCrate: //2
+                        questTitle.text = "Potion Necessities";
+                        questLog.text =
+                            "¤ O_O a crate! Let's collect the crates content."; //Destroy the crate + spawn items (potion)
+                        break;
+                    case WaterStaffQuestLine.FindingRiverByRangerArea: //3
+                        questTitle.text = "Find The River";
+                        questLog.text =
+                            "¤ The river by the Rangers camp huh. I think I noticed a partly destroyed bridge as well.";
+                        break;
+                    case WaterStaffQuestLine.SaveTheRiver: //4 //TODO Invoke changing water script
+                        questTitle.text = "Save The River!";
+                        questLog.text = "¤ Heal the river.";
+                        break;
+                    case WaterStaffQuestLine.GettingReward: //5
+                        questTitle.text = "Congratulations!";
+                        questLog.text = "¤ Collect your reward"; //Spawn staff
+                        break;
+                    case WaterStaffQuestLine.GoBackToRanger: //6
+                        questTitle.text = "Back To Ranger";
+                        questLog.text = "¤ Let's show the Ranger our new staff";
+                        break;
+                    case WaterStaffQuestLine.EndQuest:
+                        activeWaterStaffQuest = false;
+                        activeBossQuest = true;
+                        currentBossState = BossQuestLine.FindBossArea;
+                        break;
+                }
+            }
+
+            if (activeBossQuest)
+            {
+                switch (currentBossState)
+                {
+                    case BossQuestLine.FindBossArea:
+                        questTitle.text = "Find Boss Area";
+                        questLog.text = "¤ Let's follow the slime";
+                        break;
+                    case BossQuestLine.DefeatBoss:
+                        questTitle.text = "Defeat the Stone Creature!";
+                        questLog.text = "0_0 He's big... 0_0";
+                        break;
+                    case BossQuestLine.ReturnToRanger:
+                        questTitle.text = "Congratulations!";
+                        questLog.text = "¤ Let's head back to the ranger ";
+                        break;
+                }
+            }
+        }
+
+        void SetState(int part)
+        {
+            if (activeWaterStaffQuest)
+            {
+                if (part == 1)
+                    currentWaterStaffState = WaterStaffQuestLine.FindingBearMan;
+                else if (part == 2)
+                    currentWaterStaffState = WaterStaffQuestLine.CollectingCrate;
+                else if (part == 3)
+                    currentWaterStaffState = WaterStaffQuestLine.FindingRiverByRangerArea;
+                else if (part == 4)
+                    currentWaterStaffState = WaterStaffQuestLine.SaveTheRiver;
+                else if (part == 5)
+                    currentWaterStaffState = WaterStaffQuestLine.GettingReward;
+                else if (part == 6)
+                    currentWaterStaffState = WaterStaffQuestLine.GoBackToRanger;
+                else if (part == 7)
+                    currentWaterStaffState = WaterStaffQuestLine.EndQuest;
+            }
+
+            if (activeBossQuest)
+            {
+                //TODO: when invoking event. Implement quest state
+            }
+        }
+    }
+}
